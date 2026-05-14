dbf2bc35f74e948f09e07cb98e573cc4482b5941
Ayush <ayushchouhan401@gmail.com>
2026-05-14 15:05:45 +0530

    update variables file

--- backend_terraform/variables.tf
+++ backend_terraform/variables.tf
@@ -1,17 +1,15 @@
 variable "aws_region" {
   description = "The AWS region to deploy the infrastructure in"
   type        = string
-  default     = "ap-south-1" 
 }
 
 variable "mongo_uri" {
   description = "MongoDB Connection String"
   type        = string
-  default     = "mongodb+srv://ayush221018it_db_user:6fc4sZ9IZU707VN2@cluster0.w4v1jkr.mongodb.net/test"
+  sensitive   = true
 }
 
 variable "app_port" {
   description = "The port the Node.js app is running on (spotted 4000 in terminal)"
   type        = number
-  default     = 4000
 }
